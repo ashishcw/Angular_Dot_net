c4b6ec0e4b270d35c109315afa92c19fa452e244
Ashish R <ashishcw@gmail.com>
2020-06-29 11:03:54 +0530

    Updated WebSocket Extension Version to 0.1.4

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -60,6 +60,7 @@ namespace DatingApp.API.Controllers
         [HttpPost("login")]
         public async Task<IActionResult> Login(UserForLoginDTO userForLoginDTO)
         {
+
             userForLoginDTO.Username = userForLoginDTO.Username.ToLower();
 
             var userFromRepo = await _repo.Login(userForLoginDTO.Username, userForLoginDTO.Password);
